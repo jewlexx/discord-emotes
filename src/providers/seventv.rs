14cdce1d0e6de737a58229a02bad4c59b8e980c6
Juliette Cordor <grassojames5@gmail.com>
2022-09-12 18:08:10 +1000

    added public

--- src/providers/seventv.rs
+++ src/providers/seventv.rs
@@ -14,170 +14,98 @@ impl Provider for SevenTvSet {
     }
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct SevenTvSet {
-    #[serde(rename = "id")]
-    id: Id,
-
-    #[serde(rename = "name")]
-    name: String,
-
-    #[serde(rename = "tags")]
-    tags: Vec<Option<serde_json::Value>>,
-
-    #[serde(rename = "immutable")]
-    immutable: bool,
-
-    #[serde(rename = "privileged")]
-    privileged: bool,
-
-    #[serde(rename = "emotes")]
-    emotes: Vec<Emote>,
-
-    #[serde(rename = "capacity")]
-    capacity: i64,
-
-    #[serde(rename = "owner")]
-    owner: Owner,
+    pub(crate) id: Id,
+    pub(crate) name: String,
+    pub(crate) tags: Vec<Option<serde_json::Value>>,
+    pub(crate) immutable: bool,
+    pub(crate) privileged: bool,
+    pub(crate) emotes: Vec<Emote>,
+    pub(crate) capacity: i64,
+    pub(crate) owner: Owner,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Emote {
-    #[serde(rename = "id")]
-    id: String,
-
-    #[serde(rename = "name")]
-    name: String,
-
-    #[serde(rename = "flags")]
-    flags: i64,
-
+    pub(crate) id: String,
+    pub(crate) name: String,
+    pub(crate) flags: i64,
     #[serde(rename = "Timestamp")]
-    timestamp: String,
-
-    #[serde(rename = "actor_id")]
-    actor_id: Id,
-
-    #[serde(rename = "data")]
-    data: Data,
+    pub(crate) timestamp: String,
+    pub(crate) actor_id: Id,
+    pub(crate) data: Data,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Data {
-    #[serde(rename = "id")]
-    id: String,
-
-    #[serde(rename = "name")]
-    name: String,
-
-    #[serde(rename = "flags")]
-    flags: i64,
-
-    #[serde(rename = "tags")]
-    tags: Vec<Option<serde_json::Value>>,
-
-    #[serde(rename = "lifecycle")]
-    lifecycle: i64,
-
-    #[serde(rename = "listed")]
-    listed: bool,
-
-    #[serde(rename = "animated")]
-    animated: bool,
-
-    #[serde(rename = "owner")]
-    owner: Owner,
-
-    #[serde(rename = "host")]
-    host: Host,
+    pub(crate) id: String,
+    pub(crate) name: String,
+    pub(crate) flags: i64,
+    pub(crate) tags: Vec<Option<serde_json::Value>>,
+    pub(crate) lifecycle: i64,
+    pub(crate) listed: bool,
+    pub(crate) animated: bool,
+    pub(crate) owner: Owner,
+    pub(crate) host: Host,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Host {
-    #[serde(rename = "url")]
-    url: String,
-
-    #[serde(rename = "files")]
-    files: Vec<File>,
+    pub(crate) url: String,
+    pub(crate) files: Vec<File>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct File {
-    #[serde(rename = "name")]
-    name: Name,
-
-    #[serde(rename = "width")]
-    width: i64,
-
-    #[serde(rename = "height")]
-    height: i64,
-
-    #[serde(rename = "frame_count")]
-    frame_count: i64,
-
-    #[serde(rename = "size")]
-    size: i64,
-
-    #[serde(rename = "format")]
-    format: Format,
+    pub(crate) name: Name,
+    pub(crate) width: i64,
+    pub(crate) height: i64,
+    pub(crate) frame_count: i64,
+    pub(crate) size: i64,
+    pub(crate) format: Format,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Owner {
-    #[serde(rename = "id")]
-    id: String,
-
-    #[serde(rename = "username")]
-    username: String,
-
-    #[serde(rename = "display_name")]
-    display_name: String,
-
-    #[serde(rename = "roles")]
-    roles: Vec<String>,
+    pub(crate) id: String,
+    pub(crate) username: String,
+    pub(crate) display_name: String,
+    pub(crate) roles: Vec<String>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub enum Id {
     #[serde(rename = "000000000000000000000000")]
     The000000000000000000000000,
-
     #[serde(rename = "61f638a2084cfa2e05d2569b")]
     The61F638A2084Cfa2E05D2569B,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub enum Format {
     #[serde(rename = "AVIF")]
     Avif,
-
     #[serde(rename = "WEBP")]
     Webp,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub enum Name {
     #[serde(rename = "1x")]
     The1X,
-
     #[serde(rename = "1x.avif")]
     The1XAvif,
-
     #[serde(rename = "2x")]
     The2X,
-
     #[serde(rename = "2x.avif")]
     The2XAvif,
-
     #[serde(rename = "3x")]
     The3X,
-
     #[serde(rename = "3x.avif")]
     The3XAvif,
-
     #[serde(rename = "4x")]
     The4X,
-
     #[serde(rename = "4x.avif")]
     The4XAvif,
 }
