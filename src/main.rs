33d32c6b474899dfc48b02a025318c879535ca96
Juliette Cordor <grassojames5@gmail.com>
2022-09-12 17:53:07 +1000

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+use tracing_subscriber::fmt::{self, format::FmtSpan};
+
 mod providers;
+
+#[macro_use]
+extern crate log;
 
 const DEMO_ID: &str = "61f638a2084cfa2e05d2569b";
 
 fn main() {
-    println!("Hello, world!");
+    fmt::fmt()
+        .with_span_events(FmtSpan::NEW | FmtSpan::CLOSE)
+        .finish();
+
+    info!("Hello, world!");
 }
