cc30fc73764a237bf1357616fc44171574bdb26b
Juliette Cordor <grassojames5@gmail.com>
2022-09-16 13:13:23 +1000

    added twitch http client

--- src/providers/twitch.rs
+++ src/providers/twitch.rs
@@ -1,1 +1,15 @@
-pub fn get_user_id(username: impl AsRef<str>) {}
+use reqwest::header::HeaderMap;
+
+lazy_static::lazy_static! {
+    static ref TWITCH_CLIENT: reqwest::Client = {
+        let mut headers = HeaderMap::new();
+        headers.append("Client-Id", crate::dotenv_vars::CLIENT_ID.parse().unwrap());
+        headers.append("Authorization", format!("Bearer {}", crate::dotenv_vars::CLIENT_SECRET).parse().unwrap());
+
+        reqwest::Client::builder().default_headers(headers).build().unwrap()
+    };
+}
+
+pub fn get_user_id(username: impl AsRef<str>) {
+    let username = username.as_ref();
+}
