2fbd1a1c4b081da0561b5016ad1340ad8a435cf8
Juliette Cordor <grassojames5@gmail.com>
2022-09-16 13:06:23 +1000

    fix

--- src/main.rs
+++ src/main.rs
@@ -12,33 +12,15 @@ const DEMO_ID: &str = "61f638a2084cfa2e05d2569b";
 
 rotenv_codegen::dotenv_module!(visibility = "pub(crate)");
 
-fn verify_dotenv() -> Vec<&'static str> {
-    let mut missing = Vec::new();
-
-    if std::env::var("CLIENT_ID").is_err() {
-        missing.push("CLIENT_ID");
-    }
-
-    if std::env::var("CLIENT_SECRET").is_err() {
-        missing.push("CLIENT_SECRET");
-    }
-
-    missing
-}
+// Will panic if env vars do not exist
+const _: &str = dotenv_vars::CLIENT_SECRET;
+const _: &str = dotenv_vars::CLIENT_ID;
 
 fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt()
         .with_span_events(FmtSpan::ENTER | FmtSpan::EXIT)
         .with_max_level(tracing::Level::TRACE)
         .init();
-
-    {
-        let missing_env = verify_dotenv();
-
-        if !missing_env.is_empty() {
-            panic!("Missing environment variables: {:?}", missing_env);
-        }
-    }
 
     let seventv_emotes: ProviderEmotes = providers::seventv::SevenTvSet::get(DEMO_ID)?.into();
     let pwd = std::env::current_dir()?;
